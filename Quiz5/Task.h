272d99802187f67b109bf2b5f4e431a6bbc09f9d
Wes Moser <Moser.Wesley@gmail.com>
2014-03-14 14:43:28 -0400

    Initial creation of quiz.

---
+++ Quiz5/Task.h
@@ -0,0 +1,13 @@
+//
+//  Task.h
+//  Quiz5
+//
+//  Created by Moser, Wesley on 3/14/14.
+//  Copyright (c) 2014 Moser, Wesley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Task : NSObject
+
+@end
